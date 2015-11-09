06c791e2fbf9648b90b1b9313cef45a371200295
Andrew Morris <voltrevo@gmail.com>
2015-11-09 12:58:06 +1100

    Get source code from eslint instead of reading the file

--- test/rules/fileTooBig.js
+++ test/rules/fileTooBig.js
@@ -5,76 +5,61 @@ var assert = require('assert');
 var path = require('path');
 
 // community modules
-var Promise = require('bluebird');
 var range = require('range').range;
 
 // local modules
-var fileTooBig = require('../lib/rules/fileTooBig.js');
+var fileTooBig = require('../../lib/rules/fileTooBig.js');
 
-// transformed modules
-var fs = Promise.promisifyAll(require('fs'));
-var tmp = Promise.promisifyAll(require('tmp'));
+var MockContext = function(fname, content) {
+  var reports = [];
 
-var tmpDir = tmp.dirAsync({unsafeCleanup: true});
-
-var tmpFile = function(fname, content) {
-  var fpath = undefined;
-
-  return tmpDir.then(function(dpath) {
-    fpath = path.join(dpath, fname);
-    return fs.writeFileAsync(path.join(dpath, fname), content);
-  }).then(function() {
-    return fpath;
-  });
-};
-
-var context = function(fname, content) {
-  return tmpFile(fname, content).then(function(fpath) {
-    var reports = [];
-
-    return {
-      getFilename: function() {
-        return fpath;
-      },
-      options: [2, {limit: 1536}],
-      report: function() {
-        reports.push(Array.prototype.slice.apply(arguments));
-      },
-      getReports: function() {
-        return reports;
-      }
-    };
-  });
+  return {
+    getFilename: function() {
+      return path.sep + 'tmp' + path.sep + fname;
+    },
+    getSourceCode: function() {
+      return {
+        getText: function() {
+          return content;
+        }
+      };
+    },
+    options: [2, {limit: 1536}],
+    report: function() {
+      reports.push(Array.prototype.slice.apply(arguments));
+    },
+    getReports: function() {
+      return reports;
+    }
+  };
 };
 
 describe('eslint-plugin-filesize', function() {
   it('doesn\'t report anything for an empty file', function() {
-    return context('empty.js', '').then(function(context) {
-      fileTooBig(context).Program('mock-node');
-      assert.deepEqual(context.getReports(), []);
-    });
+    var context = MockContext('empty.js', '');
+    fileTooBig(context).Program('mock-node');
+    assert.deepEqual(context.getReports(), []);
   });
 
   it('doesn\'t report anything for a small file', function() {
-    return context('small.js', 'awehaioidfboaidhfboiashdobhadb').then(function(context) {
-      fileTooBig(context).Program('mock-node');
-      assert.deepEqual(context.getReports(), []);
-    });
+    var context = MockContext('small.js', 'awehaioidfboaidhfboiashdobhadb');
+    fileTooBig(context).Program('mock-node');
+    assert.deepEqual(context.getReports(), []);
   });
 
   it('reports when file is too big', function() {
-    return context(
+    var context = MockContext(
       'big.js',
       range(500000).map(function(i) {
         return String.fromCharCode(97 + (i * i * i) % 26);
       }).join('')
-    ).then(function(context) {
-      fileTooBig(context).Program('mock-node');
+    );
 
-      assert.deepEqual(
-        context.getReports(),
-        [['mock-node', 'big.min.js.gz is 2021 bytes (limit: 1536)']]
-      );
-    });
+    fileTooBig(context).Program('mock-node');
+
+    assert.deepEqual(
+      context.getReports(),
+      [['mock-node', 'big.min.js.gz is 2021 bytes (limit: 1536)']]
+    );
   });
 });
