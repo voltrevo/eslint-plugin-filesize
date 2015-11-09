87c0187e7a1a63c0ecf41905bed8503547b0e76c
Andrew Morris <voltrevo@gmail.com>
2015-11-09 11:53:34 +1100

    Empty file test

--- test/index.js
+++ test/index.js
@@ -1,10 +1,56 @@
 'use strict';
 
+// core modules
 var assert = require('assert');
-// var eslintPluginFilesize = require('../lib');
+var path = require('path');
+
+// community modules
+var Promise = require('bluebird');
+
+// local modules
+var fileTooBig = require('../lib/rules/fileTooBig.js');
+
+// transformed modules
+var fs = Promise.promisifyAll(require('fs'));
+var tmp = Promise.promisifyAll(require('tmp'));
+
+var tmpDir = tmp.dirAsync({unsafeCleanup: true});
+
+var tmpFile = function(fname, content) {
+  var fpath = undefined;
+
+  return tmpDir.then(function(dpath) {
+    fpath = path.join(dpath, fname);
+    return fs.writeFileAsync(path.join(dpath, fname), content);
+  }).then(function() {
+    return fpath;
+  });
+};
+
+var context = function(fname, content) {
+  return tmpFile(fname, content).then(function(fpath) {
+    var reports = [];
+
+    return {
+      getFilename: function() {
+        return fpath;
+      },
+      options: [2, {limit: 1536}],
+      report: function() {
+        reports.push(Array.prototype.slice.apply(arguments));
+      },
+      getReports: function() {
+        return reports;
+      }
+    };
+  });
+};
 
 describe('eslint-plugin-filesize', function() {
-  it('should have unit test!', function() {
-    assert(false, 'we expected this package author to add actual unit tests.');
+  it('doesn\'t report anything for an empty file', function() {
+    return context('empty.js', '').then(function(context) {
+      fileTooBig(context).Program('mock-node');
+      assert.deepEqual(context.getReports(), []);
+    });
   });
 });
